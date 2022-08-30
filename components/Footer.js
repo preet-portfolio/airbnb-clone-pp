32bc8e0b87e48aa63a8c0756d0ba8b812fcbddb0
Preet Panchal <PREET2020@ICLOUD.COM>
2022-08-29 20:37:10 -0400

    day one

---
+++ components/Footer.js
@@ -0,0 +1,40 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100">
+      <div className="space-y-4 text-xs text-gray-600">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investor</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>It is clone website</p>
+        <p>Using NextJS</p>
+        <p>Styled with Tailwind❤️</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT MYSELF</h5>
+        <p>i am a full stack web developer</p>
+        <p>Github👾</p>
+        <p>Portfolio</p>
+        <p>LinkedIn</p>
+        <p>Canada🇨🇦</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Centre</p>
+        <p>Trust & Safety</p>
+        <p>Customer Support</p>
+        <p>Awards</p>
+        <p>For Developers</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
